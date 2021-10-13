a90a5d59e1150583c1d7ad4e2c8a01b5dfc2ea06
Kyron Meza <kyron.meza@xerevro.com>
2021-10-13 17:58:37 -0300

    Agregar archivos de proyecto.

---
+++ TestAceptacionEjemplo/Features/SampleToDoApp.feature
@@ -0,0 +1,12 @@
+﻿Feature: Sample ToDo App
+
+@ToDoApp
+Scenario: Añadir un Elemento a la Lista
+	Given que estoy situado en la Sample ToDo App
+	Then seleccionar el Primer Item
+	Then seleccionar el Segundo Item
+	Then encontrar el cuadro de texto para ingresar un nuevo item (escribir)
+	Then hacer click en el Submit
+	And  verificar si el item fue o no creado en la lista
+	Then cerrar el navegador
+
---
+++ TestAceptacionEjemplo/Steps/SampleToDoAppStepDefinition.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Edge.SeleniumTools;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace TestAceptacionEjemplo.Steps
+{
+    [Binding]
+    public sealed class SampleToDoAppStepDefinition
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        IWebDriver _driver;
+
+        string urlDestino = "https://lambdatest.github.io/sample-todo-app/";
+
+        string nuevoElemento = "TEXTO";
+
+        public SampleToDoAppStepDefinition(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"que estoy situado en la Sample ToDo App")]
+        public void GivenQueEstoySituadoEnLaSampleToDoApp()
+        {
+            //INICIALIZAR EL DRIVER DEL NAVEGADOR
+            //Utilizando Edge Chromium
+            EdgeOptions edgeOptions = new EdgeOptions();
+            edgeOptions.UseChromium = true;
+            _driver = new EdgeDriver(edgeOptions);
+            
+            _driver.Manage().Window.Maximize();
+            
+            _driver.Url = urlDestino;
+
+            System.Threading.Thread.Sleep(2000);
+
+        }
+
+        [Then(@"seleccionar el Primer Item")]
+        public void ThenSeleccionarElPrimerItem()
+        {
+            //Hacer Click en el Checkbox del primer Item
+            IWebElement primerElemento = _driver.FindElement(By.Name("li1"));
+            primerElemento.Click();
+
+        }
+
+        [Then(@"seleccionar el Segundo Item")]
+        public void ThenSeleccionarElSegundoItem()
+        {
+            //Hacer Click en el Checkbox del segundo Item
+            IWebElement segundoElemento = _driver.FindElement(By.Name("li2"));
+            segundoElemento.Click();
+        }
+
+        [Then(@"encontrar el cuadro de texto para ingresar un nuevo item \(escribir\)")]
+        public void ThenEncontrarElCuadroDeTextoParaIngresarUnNuevoItemEscribir()
+        {
+            IWebElement cuadroTexto = _driver.FindElement(By.Id("sampletodotext"));
+            cuadroTexto.SendKeys(nuevoElemento);
+        }
+
+        [Then(@"hacer click en el Submit")]
+        public void ThenHacerClickEnElSubmit()
+        {
+            IWebElement submit = _driver.FindElement(By.Id("addbutton"));
+            submit.Click();
+        }
+
+        [Then(@"verificar si el item fue o no creado en la lista")]
+        public void ThenVerificarSiElItemFueONoCreadoEnLaLista()
+        {
+            IWebElement nuevoItem = _driver.FindElement(By.XPath("/html/body/div/div/div/ul/li[6]/span"));
+            string nuevoItemString = nuevoItem.Text;
+
+            System.Threading.Thread.Sleep(2000);
+
+            Assert.AreEqual(nuevoElemento, nuevoItemString);
+
+            System.Threading.Thread.Sleep(2000);
+
+        }
+
+        [Then(@"cerrar el navegador")]
+        public void ThenCerrarElNavegador()
+        {
+            _driver.Quit();
+        }
+    }
+}
